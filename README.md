96cb1ec5bb3813f663156420f9bfb98451754e83
Crazibeat <crazibeatdeveloper@gmail.com>
2025-06-26 04:52:45 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,36 +1,39 @@
-# 3D Card Swipe Implementation
-Special thanks to [Kostia Sokolovskyi](https://x.com/k_sokolovskyi) for helping out with some of the logic implementation
+<!--
+This README describes the package. If you publish this package to pub.dev,
+this README's contents appear on the landing page for your package.
 
-## Video Demo
+For information about how to write a good package README, see the guide for
+[writing package pages](https://dart.dev/tools/pub/writing-package-pages).
 
+For general information about developing packages, see the Dart guide for
+[creating packages](https://dart.dev/guides/libraries/create-packages)
+and the Flutter guide for
+[developing packages and plugins](https://flutter.dev/to/develop-packages).
+-->
 
-https://github.com/user-attachments/assets/7a23c49e-b256-41a2-a9a5-157af1867903
+TODO: Put a short description of the package here that helps potential users
+know whether this package might be useful for them.
 
+## Features
 
+TODO: List what your package can do. Maybe include images, gifs, or videos.
 
-## Installation
-To run this app locally, follow these steps:
+## Getting started
 
-1. Clone the repo locally
+TODO: List prerequisites and provide or point to information on how to
+start using the package.
 
-```bash
-git clone https://github.com/danielkiing3/path_animation.git
-cd headspace_onboarding
+## Usage
+
+TODO: Include short and useful examples for package users. Add longer examples
+to `/example` folder.
+
+```dart
+const like = 'sample';
 ```
-2. Install dependencies:
-```bash
-flutter pub get
-```
-3. Run the app:
-```bash
-flutter run
-```
 
-# 💬 Contact
-If you have any questions or feedback, feel free to reach out:
+## Additional information
 
-Email: oluwaferanmiidaniel@gmail.com
-
-GitHub: [danielkiing3](https://github.com/danielkiing3)
-
-Twitter: [@daniel_kiing3](https://twitter.com/daniel_kiing3)
+TODO: Tell users more about the package: where to find more information, how to
+contribute to the package, how to file issues, what response they can expect
+from the package authors, and more.
---
+++ example/lib/main.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+import "package:stacked_carousel/stacked_carousel.dart";
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Flutter Demo', home: Example());
+  }
+}
+
+class Example extends StatefulWidget {
+  const Example({super.key});
+
+  @override
+  State<Example> createState() => _ExampleState();
+}
+
+class _ExampleState extends State<Example> {
+  FancyStackCarouselController carouselController =
+      FancyStackCarouselController();
+
+  bool autoPlay = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          FancyStackCarousel(
+            items: [
+              FancyStackItem(
+                id: 1,
+                color: Colors.red,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+              FancyStackItem(
+                id: 2,
+                color: Colors.green,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+              FancyStackItem(
+                id: 3,
+                color: Colors.blue,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+              FancyStackItem(
+                id: 4,
+                color: Colors.yellow,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.yellow,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+              FancyStackItem(
+                id: 5,
+                color: Colors.purple,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.purple,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+            ],
+            carouselController: carouselController,
+            options: FancyStackCarouselOptions(
+              size: Size(250, 150),
+              autoPlay: autoPlay,
+              autoPlayInterval: Duration(seconds: 2),
+              autoPlayAnimationDuration: Duration(milliseconds: 1000),
+              onPageChanged: (index, reason) =>
+                  print("Index change: ${index}, Reason: ${reason}"),
+            ),
+          ),
+          SizedBox(height: 80),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              spacing: 20,
+              children: [
+                Expanded(
+                  child: FilledButton(
+                    onPressed: () {
+                      carouselController.animateToLeft();
+                    },
+                    child: Text("Prev"),
+                  ),
+                ),
+                Expanded(
+                  child: FilledButton(
+                    onPressed: () {
+                      carouselController.animateToRight();
+                    },
+                    child: Text("Next"),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          FilledButton(
+            onPressed: () {
+              setState(() {
+                autoPlay = !autoPlay;
+              });
+            },
+            child: Text("Toggle AutoPlay ${autoPlay}"),
+          ),
+        ],
+      ),
+    );
+  }
+}
